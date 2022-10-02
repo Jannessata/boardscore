46f066c3c3589ae17fbe6653393b063f7b5867ec
Janna <zhnn1603@gmail.com>
2022-10-02 19:11:00 +0500

    Загрузила файлы

---
+++ app.js
@@ -0,0 +1,16 @@
+let scoreplus1 = document.getElementsById('score1')
+    console.log(scoreplus1)
+
+let scoreplus2 = document.getElementsById('score2')
+
+let count = 0
+
+function plus1() {
+   count = count + 1
+   scoreplus1.innerText = count
+}
+
+
+
+
+
